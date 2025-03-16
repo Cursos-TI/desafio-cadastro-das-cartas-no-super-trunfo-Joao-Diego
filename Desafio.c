7fd1031e74d31d6bfd49e64b169101219152a449
Joao-Diego <diegotonha8@gmail.com>
2025-03-16 15:02:10 +0000

    Nme

--- Desafio.c
+++ Desafio.c
@@ -9,5 +9,21 @@ int main (){
     float PIB;
     int numero;
 
-    
+    printf("Nome do Estado: ");
+    scanf("%d", &Estado);
+    printf("Nome do Estado é: %d \n", Estado);
+
+    printf("Nome da Cidade: ");
+    scanf("%d", &Nome);
+    printf("Nome da Cidade é: %d \n", Nome);
+
+    printf("Codigo da Carta: ");
+    scanf("%s", &codigo);
+    printf("O cogigo é: %s \n", codigo);
+
+    printf("Número da População: ");
+    scanf("%f", população);
+    printf("Número da População é: %f \n", população);
+
+
 }
